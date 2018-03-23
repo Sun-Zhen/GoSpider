87330fedac7b50c26c516fddc729523a35750898
Alden <Alden@Alden-2.local>
2018-03-23 19:52:26 +0800

    init dev

--- bin/start.sh
+++ bin/start.sh
@@ -3,6 +3,10 @@
 BIN_PATH=$(cd $(dirname $0) && pwd)
 HOME_PATH=$(cd ${BIN_PATH} && cd .. && pwd)
 export GOPATH=${HOME_PATH}
+
+go get gopkg.in/yaml.v2
+go get github.com/golang/glog
+go get github.com/PuerkitoBio/goquery
 go install -v spider
 ${BIN_PATH}/spider -log_dir ./logs
 
